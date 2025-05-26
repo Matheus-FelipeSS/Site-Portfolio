3790e267fa2b60f3f1cf79fdfa2f4ed64823eb79
Matheus Felipe Silvério da Silveira <matheusf.silveira@hotmail.com>
2025-05-25 23:53:53 -0300

    ajuste no js

--- js/script.js
+++ js/script.js
@@ -12,18 +12,3 @@ function enviaWhatsApp(event) {
 
   window.open(url, "_blank");
 }
-
-function enviaEmail(event) {
-  event.preventDefault();
-
-  const nome = document.getElementById("nome").value;
-  const mensagem = document.getElementById("mensagem").value;
-  const emailDestino = "matheusf.silveira@hotmail.com";
-
-  const assunto = encodeURIComponent(`Contato do Portfólio - ${nome}`);
-  const corpo = encodeURIComponent(`Olá! Me chamo ${nome},\n\n${mensagem}`);
-
-  const url = `mailto:${emailDestino}?subject=${assunto}&body=${corpo}`;
-
-  window.open(url, "_blank");
-}
